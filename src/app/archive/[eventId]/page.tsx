5d5a9f6705be47f5a98d726b0bc5190f341da3d7
shelleyhowarth <37180741+shelleyhowarth@users.noreply.github.com>
2026-02-17 01:31:49 +0000

    feat: add photogallery to archive event pages

--- src/app/archive/[eventId]/page.tsx
+++ src/app/archive/[eventId]/page.tsx
@@ -3,6 +3,9 @@ import { EventRecap } from "@/components/event-recap/event-recap";
 import { PageTitle } from "@/components/page-title/page-title";
 import styles from "./eventId.module.css";
 import { eventData } from "./eventId.data";
+import "react-photo-album/styles.css";
+import { PhotoGallery } from "@/components/photo-gallery/photo-gallery";
+
 interface PageProps {
     params: Promise<{
         eventId: string;
@@ -11,16 +14,38 @@ interface PageProps {
 
 export default async function ArchiveEventPage({ params }: PageProps) {
     const { eventId } = await params;
-    const eventDetails = eventData.find(event => event.id === eventId);
+    const eventDetails = eventData.find((event) => event.id === eventId);
     const { date = "", venue = "", lineup = [] } = eventDetails || {};
+
+    async function getImages() {
+        const prefix = `archive-gallery/${eventId}/`;
+        const url = `https://shelling-it.s3.eu-north-1.amazonaws.com/?list-type=2&prefix=${encodeURIComponent(prefix)}`;
+        const res = await fetch(url);
+        const text = await res.text();
+
+        const keys = Array.from(
+            text.matchAll(/<Key>(.*?)<\/Key>/g),
+            match => match[1]
+        );
+
+        return keys.map(
+            key => `https://shelling-it.s3.amazonaws.com/${key}`
+        );
+    }
+
+    const images = await getImages()
+
 
     return (
         <>
             <div className={styles.topContainer}>
                 <PageTitle line1={"EVENT"} line2={eventId} />
                 <div className={styles.recapContainer}>
-                    <EventRecap date={date} venue={venue} lineup={lineup}  />
+                    <EventRecap date={date} venue={venue} lineup={lineup} />
                 </div>
+            </div>
+            <div className={styles.galleryContainer}>
+                <PhotoGallery images={images} />
             </div>
         </>
     );
