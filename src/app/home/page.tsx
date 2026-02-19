062ff8d0d8f319afa96142abbdaefb258705694d
shelleyhowarth <37180741+shelleyhowarth@users.noreply.github.com>
2026-02-19 21:27:15 +0000

    feat: add logo to header and mobile home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,15 @@
 import { NextEvent } from '@/components/next-event/next-event';
-import Head from "next/head";
 import styles from './home.module.css';
-import type { Metadata } from 'next'
-
+import Logo from '../../../public/logo-004.webp'
 
 export default function Home() {
     return (
         <>
             <title>SHELLING.IT</title>
+            <div className={styles.logoContainer}>
+                <img src={Logo.src} alt="SHELLING.IT Logo" />
+            </div>
+
             <div className={styles.nextEventContainer}>
                 <NextEvent date={'3 MAY 2026'} venue={'THE DBA, MANCHESTER'} headliner={'TBA'} ticketUrl={'https://ra.co/events/2334586'} />
             </div>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,8 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import styles from './header.module.css';
+import Logo from '../../../public/logo-004.webp'
+import LogoShort from '../../../public/logo-004-short.webp'
 
 export const Header = () => {
     const pathname = usePathname();
@@ -11,10 +13,10 @@ export const Header = () => {
             <div className={styles.container}>
                 <div className={styles.titleSlot}>
                     <Link href='/' className={styles.titleDesktop}>
-                        <h1>SHELLING.IT +</h1>
+                        <img src={Logo.src} alt="SHELLING.IT Logo" className={styles.logo} />
                     </Link>
                     <Link href='/' className={styles.titleMobile}>
-                        <h1>+</h1>
+                        <img src={LogoShort.src} alt="SHELLING.IT Logo" className={styles.logo} />
                     </Link>
                 </div>
                 <div className={styles.nav}>
