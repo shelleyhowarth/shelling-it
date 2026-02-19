5434f5b436c43f6f49a26a4bb1cdd904de9e683e
shelleyhowarth <37180741+shelleyhowarth@users.noreply.github.com>
2026-02-19 20:48:02 +0000

    refactor: use metadata object instead of nextjs/head

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,16 @@
 import { NextEvent } from '@/components/next-event/next-event';
 import Head from "next/head";
 import styles from './home.module.css';
+import type { Metadata } from 'next'
 
+ 
+export const metadata: Metadata = {
+  title: 'SHELLING.IT',
+}
+ 
 export default function Home() {
     return (
         <>
-            <Head>
-                <title>SHELLING.IT</title>
-            </Head>
             <div className={styles.nextEventContainer}>
                 <NextEvent date={'3 MAY 2026'} venue={'THE DBA, MANCHESTER'} headliner={'TBA'} ticketUrl={'https://ra.co/events/2334586'} />
             </div>
